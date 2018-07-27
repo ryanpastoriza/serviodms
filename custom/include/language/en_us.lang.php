f26f0e844f339cf667aee7dafca50117491dc85d
sade2041 <bryanjeciebahala@gmail.com>
2018-07-27 14:39:39 +0800

    for update

---
+++ custom/include/language/en_us.lang.php
@@ -0,0 +1,3 @@
+<?php
+$app_strings['LBL_GROUPTAB1_1532672985'] = 'Dependencies';
+?>
---
+++ custom/modules/j41_base_model/language/en_us.lang.php
@@ -0,0 +1,5 @@
+<?php
+// created: 2018-07-27 08:37:29
+$mod_strings = array (
+  'LBL_NAME' => 'Lead Source',
+);
