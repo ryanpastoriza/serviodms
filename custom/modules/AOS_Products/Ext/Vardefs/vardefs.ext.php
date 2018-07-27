0c1b295b1305fca84612d24d19a13ea735ab27f5
iDale <blanco.iandale@gmail.com>
2018-07-27 15:27:23 +0800

    ProspectInquiry2

---
+++ custom/modules/AOS_Products/Ext/Vardefs/vardefs.ext.php
@@ -0,0 +1,10 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+
+ // created: 2018-07-27 09:13:19
+$dictionary['AOS_Products']['fields']['body_type_c']['inline_edit']='1';
+$dictionary['AOS_Products']['fields']['body_type_c']['labelValue']='Body Type';
+
+ 
+?>
---
+++ custom/modules/AOS_Products/language/en_us.lang.php
@@ -0,0 +1,5 @@
+<?php
+// created: 2018-07-27 09:13:19
+$mod_strings = array (
+  'LBL_BODY_TYPE' => 'Body Type',
+);
