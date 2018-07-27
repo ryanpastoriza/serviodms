757e751a3d08b8b8abab4d340337d25c8eae3390
sade2041 <bryanjeciebahala@gmail.com>
2018-07-27 15:19:24 +0800

    for another update

--- custom/application/Ext/TableDictionary/tabledictionary.ext.php
+++ custom/application/Ext/TableDictionary/tabledictionary.ext.php
@@ -30,4 +30,5 @@ include('custom/metadata/jump_quote_jump_discount_1MetaData.php');
  //WARNING: The contents of this file are auto-generated
 include('custom/metadata/jump_quote_jump_insurancecharges_1MetaData.php');
 
+
 ?>
--- custom/modules/m20_Opportunity/language/en_us.lang.php
+++ custom/modules/m20_Opportunity/language/en_us.lang.php
@@ -1,11 +1,23 @@
 <?php
-// created: 2018-07-26 05:55:48
+// created: 2018-07-27 09:13:51
 $mod_strings = array (
-  'LBL_EDITVIEW_PANEL2' => 'New  Opportunity',
+  'LBL_EDITVIEW_PANEL2' => 'New Opportunity',
   'LBL_EDITVIEW_PANEL1' => 'New Panel 1',
   'LBL_EDITVIEW_PANEL3' => 'New Panel 3',
   'LBL_EDITVIEW_PANEL4' => 'New Panel 4',
   'LBL_EDITVIEW_PANEL5' => 'New Panel 5',
   'LBL_EDITVIEW_PANEL6' => 'New Panel 6',
   'LBL_EDITVIEW_PANEL7' => 'New Panel 7',
+  'LBL_BUDGET_AMOUNT' => 'Budget Amount',
+  'LBL_PURCHASED_TIMEFRAME' => 'purchased timeframe',
+  'LBL_ESTIMATED_CLOSEDATE' => 'Estimated Close Date',
+  'LBL_OPPORTUNITYID' => 'Opportunity ID',
+  'LBL_CUSTOMER_JUMP_PROSPECT_INQUIRY_ID' => 'customer (related  ID)',
+  'LBL_CUSTOMER' => 'customer',
+  'LBL_LEAD_SOURCE_J41_LEAD_SOURCE_ID' => 'Lead Source (related  ID)',
+  'LBL_LEAD_SOURCE' => 'Lead Source',
+  'LBL_BASE_MODEL_J41_BASE_MODEL_ID' => 'Base Model (related  ID)',
+  'LBL_BASE_MODEL' => 'Base Model',
+  'LBL_COLOR_J41_COLOR_ID' => 'Color (related  ID)',
+  'LBL_COLOR' => 'Color',
 );
