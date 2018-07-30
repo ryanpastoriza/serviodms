d0b219259568545ace377811126c7fe4048e6fa0
ryanpastoriza <ryanpastoriza@gmail.com>
2018-07-30 14:39:36 -0700

    created dealer and branch modules

---
+++ custom/modules/jump_dealer/language/en_us.lang.php
@@ -0,0 +1,12 @@
+<?php
+// created: 2018-07-30 18:43:16
+$mod_strings = array (
+  'LBL_NAME' => 'Dealer',
+  'LBL_DEALER_CODE' => 'Dealer Code',
+  'LBL_EMAIL_ADDRESS' => 'Email Address',
+  'LBL_TIN' => 'TIN',
+  'LBL_OWNING_BRANCH' => 'Owning Branch',
+  'LBL_EDITVIEW_PANEL3' => 'Overview',
+  'LBL_DETAILVIEW_PANEL2' => 'Overview',
+  'LBL_STATUS' => 'Status',
+);
