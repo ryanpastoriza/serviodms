ba466b66554b1e268efc8186c31af9f69ffeb197
ryanpastoriza <ryanpastoriza@gmail.com>
2018-07-31 10:31:57 -0700

    made some repairs

--- custom/application/Ext/TableDictionary/tabledictionary.ext.php
+++ custom/application/Ext/TableDictionary/tabledictionary.ext.php
@@ -41,9 +41,4 @@ include('custom/metadata/m20_opportunity_jump_quote_1MetaData.php');
 include('custom/metadata/m20_opportunity_m20_activity_1MetaData.php');
 
 
- 
- //WARNING: The contents of this file are auto-generated
-include('custom/metadata/m20_opportunity_jump_quote_1MetaData.php');
-
-
 ?>
